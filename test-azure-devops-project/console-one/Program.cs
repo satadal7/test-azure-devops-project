762d2be32b73cd5fd46bcc27112f247c176393b0
brunocerecetto <bruno.cerecetto@gmail.com>
2018-11-16 11:37:35 -0300

    adding porjects

---
+++ test-azure-devops-project/console-one/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace console_one
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
